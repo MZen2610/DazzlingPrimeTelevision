68b33f57122a75eac358c0fbfd05363213f52779
MZen2610 <mzen1980@gmail.com>
2022-05-09 15:35:58 +0300

    Create quiz_questions.py

---
+++ quiz_questions.py
@@ -0,0 +1,22 @@
+import os
+
+
+def make_questions_answers(folder):
+    folder = os.path.join(os.path.dirname(__file__), folder)
+    all_questions_answers = {}
+    for filename in os.listdir(folder):
+        with open(os.path.join(folder, filename), 'rt', encoding='KOI8-R') as file:
+            content = file.read().split('\n\n')
+        questions = []
+        answers = []
+        for line in content:
+            if line.startswith('Вопрос'):
+                questions.append(' '.join(line.split('\n')[1:]))
+            if line.startswith('Ответ'):
+                answers.append(' '.join(line.split('\n')[1:]))
+        questions_answers = dict(zip(questions, answers))
+        all_questions_answers.update(questions_answers)
+    return all_questions_answers
+
+
+
