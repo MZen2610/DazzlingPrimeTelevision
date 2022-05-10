d79255f260e61a840a0d5b6354dbf6b51658ef11
MZen2610 <mzen1980@gmail.com>
2022-05-10 15:48:13 +0300

    Update quiz_questions.py

--- quiz_questions.py
+++ quiz_questions.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 
 def make_questions_answers(folder):
@@ -6,17 +7,22 @@ def make_questions_answers(folder):
     all_questions_answers = {}
     for filename in os.listdir(folder):
         with open(os.path.join(folder, filename), 'rt', encoding='KOI8-R') as file:
-            content = file.read().split('\n\n')
+            contents = file.read().split('\n\n')
         questions = []
         answers = []
-        for line in content:
-            if line.startswith('Вопрос'):
-                questions.append(' '.join(line.split('\n')[1:]))
-            if line.startswith('Ответ'):
-                answers.append(' '.join(line.split('\n')[1:]))
+        for content in contents:
+            line = ' '.join(content.split('\n'))
+            if ''.join(line.split(' ')).startswith('Вопрос'):
+                questions.append(' '.join(line.split(':')[1:]))
+            if ''.join(line.split(' ')).startswith('Ответ'):
+                answers.append(' '.join(line.split(':')[1:]))
         questions_answers = dict(zip(questions, answers))
         all_questions_answers.update(questions_answers)
     return all_questions_answers
+
+def multi_split(delimiters, string, maxsplit=0):
+    regex_pattern = '|'.join(map(re.escape, delimiters))
+    return re.split(regex_pattern, string, maxsplit)[0]
 
 
 
